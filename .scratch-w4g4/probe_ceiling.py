d0e73799ae87c61c1f31209ff9a61e122b6dad7e
Sean Reilly <qwv4851@gmail.com>
2026-08-30 22:32:51 -0700

    chore: keep the W4G4 scratch directory out of the branch

--- .scratch-w4g4/probe_ceiling.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from engine.cost_x_definitions import cast_x_ceiling_line, caps_cast_x
-print(cast_x_ceiling_line("X can't be greater than the number of snow lands you control."))
-print(caps_cast_x("X can't be greater than the number of snow lands you control.\nfoo"))
-print(cast_x_ceiling_line("X can't be greater than the number of blorps you control."))
